a627d32fb038247a7cfa4498ff4b88287f78b0f2
Shreya Goyal <shreyagoyal401@gmail.com>
2025-06-29 00:43:45 +0530

    Delete old frontend directory

--- frontend/src/api/clothing.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000/api/clothing', withCredentials: true });
-
-export const fetchClothing = () => API.get('/all');
-export const addClothing = (data) => API.post('/add', data);
-export const deleteClothing = (id) => API.delete(`/delete/${id}`);
-export const fetchNotifications = () => API.get('/notifications');
