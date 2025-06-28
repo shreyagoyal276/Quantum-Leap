6cad1f590fe56e1cdd0794b8a35736098482d40b
Shreya Goyal <shreyagoyal401@gmail.com>
2025-06-29 00:43:32 +0530

    Delete old backend directory

--- backend/routes/billing.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { addBillingEntry, getBillingHistory } = require('../controllers/billing.controller.js');
-
-const router = express.Router();
-
-router.post('/', addBillingEntry);
-router.get('/history', getBillingHistory);
-
-module.exports = router;
